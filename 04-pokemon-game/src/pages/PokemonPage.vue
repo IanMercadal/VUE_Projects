6aedc7e26c7d0ecb33198edcff9106be0238a729
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-02-07 19:33:40 +0100

    feat: :art: 04 - Pokemon Game - Creando Petición API
    Realizando consulta a una API pokemon y obtenemos data de la respuesta

--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -11,7 +11,7 @@ import PokemonOptions from '../components/PokemonOptions'
 import PokemonPicture from '../components/PokemonPicture'
 
 import getPokemonOptions from '@/helpers/getPokemonOptions'
-console.log( getPokemonOptions() )
+// console.log( getPokemonOptions() )
 
 export default {
     components: { PokemonOptions, PokemonPicture}
