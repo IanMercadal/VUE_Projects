e9e870f8a606d401aa83c97b4777b9e236d80e95
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-02-08 18:50:43 +0100

    feat: :art: 04 - Pokemon Game - Unit Test - PokemonPicture
    Validación en caso de que la imagen se oculte y el resultado se muestre

--- 04-pokemon-game/tests/unit/components/PokemonPicture.spec.js
+++ 04-pokemon-game/tests/unit/components/PokemonPicture.spec.js
@@ -11,5 +11,40 @@ describe('PokemonPicture component', () => {
         })
 
         expect(wrapper.html() ).toMatchSnapshot() 
+        expect( img1.attributes('src') ).toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/100.svg')
+
+    })
+    test('Debe mostrar la imagen oculta y el pokemon 100', () => {
+        
+        const wrapper = shallowMount(PokemonPicture, {
+            props: {
+                pokemonId: 100,
+                showPokemon: false
+            }
+        })
+
+        const [img1, img2] = wrapper.findAll('img')
+
+        expect( img1.exists() ).toBeTruthy()
+        expect(img2).toBe(undefined)
+
+        expect(img1.classes('hidden-pokemon')).toBe(true)
+    })
+
+    test('Debe mostrar el pokemon si showPokemon:true', () => {
+        
+        const wrapper = shallowMount(PokemonPicture, {
+            props: {
+                pokemonId: 100,
+                showPokemon: false
+            }
+        })
+
+        const img1 = wrapper.find('img')
+
+        expect( img1.exists() ).toBeTruthy()
+
+        expect(img1.classes('hidden-pokemon')).toBe(false)
+        expect(img1.classes('fade-in')).toBe(true)
     })
 })
