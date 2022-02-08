e6e66e78ba8d3b526b0e3ce2223556d9f02035d3
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-02-08 17:45:14 +0100

    feat: :art: 04 - Pokemon Game - Unit test obtención de atributos pokemons
    Comprobando name e id de los pokemons obtenidos

--- 04-pokemon-game/tests/unit/helpers/getPokemonOptions.spec.js
+++ 04-pokemon-game/tests/unit/helpers/getPokemonOptions.spec.js
@@ -1,4 +1,4 @@
-import {getPokemons} from '@/helpers/getPokemonOptions'
+import getPokemonOptions, {getPokemons, getPokemonNames} from '@/helpers/getPokemonOptions'
 
 describe('getPokemonOptions helpers', () => {
     test('debe regresar un array de números', () => {
@@ -9,5 +9,41 @@ describe('getPokemonOptions helpers', () => {
         expect(pokemons[500]).toBe(501)
         expect(pokemons[649]).toBe(650)
 
+    })
+
+    test('debe retornar array de 4 elementos con nombres de pokemons', async () => {
+
+        const pokemons = await getPokemonNames([1,2,3,4])
+        
+        expect(pokemons).toStrictEqual([
+            {name: 'bulbasaur', id: 1},
+            {name: 'ivysaur', id: 2},
+            {name: 'venusaur', id: 3},
+            {name: 'charmander', id: 4},
+        ])
+    })
+    test('getPokemonOptions debe retornar un array mezclado', async () => {
+
+        const pokemons = await getPokemonOptions()
+
+        expect(pokemons.length).toBe(4)
+        expect(pokemons).toEqual([
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            },
+            {
+                name: expect.any(String),
+                id: expect.any(Number)
+            }
+        ])
     })
 })
