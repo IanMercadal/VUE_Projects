a141aef1187ad0f8ce01b5290c45335aa43b21cd
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-03-01 12:45:21 +0100

    feat: :art: 05 - Rutas y ciclos - Creando Navbar
    Creando navbar con LazyLoad

---
+++ 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link to="/">Pokemon List</router-link>
+        <router-link to="/id">Pokemon ID</router-link>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<style scoped>
+div {
+    padding: 0 30p;
+}
+
+div a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 0 10px;
+}
+</style>
